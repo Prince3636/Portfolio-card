3ee46587d546d2cdeb5662dc442a5131402480ec
Prince Kumar <raj722663@gmail.com>
2026-03-20 12:33:18 +0530

    Initial commit - portfolio card

---
+++ Animated_Card/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("contextmenu", function(e) {
+  e.preventDefault();
+});
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "F12") {
+    e.preventDefault();
+  }
+
+  if (e.ctrlKey && e.shiftKey && e.key === "I") {
+    e.preventDefault();
+  }
+});
